3d0d86c2641bbd6892f50c065833d08d4f8136eb
Jerald Austero <jerald.austero0229@gmail.com>
2025-05-26 14:56:31 +0800

    Add requirement
    Separete blog module
    
    Update gitignore

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -15,9 +22,22 @@ def unpublished():
 def about(id: int):
     # fetch blog with id = id
     return {'data': id }
-
+ 
 
 @app.get('/blog/{id}/comments')
 def comments(id: int):
     # fetch comments of blog with id = id
     return {'data': {'1', '2', '3'} }
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data' : f'Blog is created with title as {request.title}'}
+
+# Debugging purpose
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=9000)
